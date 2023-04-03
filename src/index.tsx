ace9d01e37a5cb786336dde1999ca54dd360e685
Himanshu Gunwant <hgunwant2312@gmail.com>
2023-04-03 15:07:23 +0530

    improvements 😇

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import * as Expo from 'expo';
 import RootTab from '@routes/rootTab';
-import HomeScreen from '@screens/HomeScreen';
-import LoginScreen from '@screens/LoginScreen';
+import HomeStack from '@routes/homeStack';
+import Posts from '@pages/Posts';
+import Post from '@pages/Post';
+import LoginStack from '@routes/loginStack';
+import SigninPage from '@pages/SigninPage';
+import SignupPage from '@pages/SignupPage';
 
 function App() {
   return (
@@ -13,8 +17,24 @@ function App() {
           headerShown: false,
         }}
       >
-        <RootTab.Screen name="Home" component={HomeScreen} />
-        <RootTab.Screen name="Login" component={LoginScreen} />
+        <RootTab.Screen
+          name="Home"
+          component={() => (
+            <HomeStack.Navigator>
+              <HomeStack.Screen name="Posts" component={Posts} />
+              <HomeStack.Screen name="Post" component={Post} />
+            </HomeStack.Navigator>
+          )}
+        />
+        <RootTab.Screen
+          name="Login"
+          component={() => (
+            <LoginStack.Navigator>
+              <LoginStack.Screen name="Signin" component={SigninPage} />
+              <LoginStack.Screen name="Signup" component={SignupPage} />
+            </LoginStack.Navigator>
+          )}
+        />
       </RootTab.Navigator>
     </NavigationContainer>
   );
